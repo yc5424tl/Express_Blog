db46a1873f284caf475d29e6585da31128466a1c
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-06-14 16:09:33 -0500

    commit before starting a new branch for authentication

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,21 @@
-let express = require('express');
-const passport = require("passport");
-let router = express.Router();
-
-
-router.get('/auth/google',
-    passport.Passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
-
-router.get('/auth/google/callback',
-    passport.Passport.authenticate('google', {failureRediret: '/' }),
-    function(req, res) {
-        res.redirect('/');
-    });
-
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
-module.exports = router;
+// let express = require('express');
+// const passport = require("passport");
+// let router = express.Router();
+//
+//
+// router.get('/auth/google',
+//     passport.Passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+//
+// router.get('/auth/google/callback',
+//     passport.Passport.authenticate('google', {failureRediret: '/' }),
+//     function(req, res) {
+//         res.redirect('/');
+//     });
+//
+//
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//     res.send('respond with a resource');
+// });
+//
+// module.exports = router;
